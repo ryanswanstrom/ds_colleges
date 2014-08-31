bfab29ae18d2d8fb93618c0fd842cc1229fe4b63
Ryan <swGooF@users.noreply.github.com>
2014-08-31 15:11:27 +0000

    updating the table

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,7 @@
 class School < ActiveRecord::Base
   has_many :school_extras
   validates_formatting_of :url, using: :url
+  validates_presence_of :name, :url, :degree, :program
+  validates_uniqueness_of :url
+  validates_inclusion_of  :degree, :in => %w( Masters, Bachelors, Doctorate, Certificate )
 end
