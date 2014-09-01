0f0f28af38ba0bf44724212adae7343196d529ba
Ryan <swGooF@users.noreply.github.com>
2014-09-01 13:26:36 +0000

    fixed possible degree values

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,5 +3,5 @@ class School < ActiveRecord::Base
   validates_formatting_of :url, using: :url
   validates_presence_of :name, :url, :degree, :program
   validates_uniqueness_of :url
-  validates_inclusion_of  :degree, :in => %w( Masters, Bachelors, Doctorate, Certificate )
+  validates_inclusion_of  :degree, :in => %w( Masters Bachelors Doctorate Certificate )
 end
