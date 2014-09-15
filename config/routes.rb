6fb9e52a2c43d84d3b2f9d173bb18298a99fe8b2
Ryan <swGooF@users.noreply.github.com>
2014-09-15 23:30:10 +0000

    updated school

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'schools', to: 'schools#index'
+  get 'colleges', to: 'schools#index'
+  get 'bootcamps', to: 'bootcamps#bootcamps'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'schools#index'
+  root 'application#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
